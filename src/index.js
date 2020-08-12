272004c93d87a1a7cf0251d9283ce2ec970312be
rohan <rohanrajgautam@gmail.com>
2020-08-12 21:48:51 +0545

    added redux-persist for localStorage

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 import App from "./App";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </Router>
   </Provider>,
   document.getElementById("root")
