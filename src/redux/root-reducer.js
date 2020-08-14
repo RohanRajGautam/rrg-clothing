120068402f2ed332dc9d2fcf7069a606fa05d913
rohan <rohanrajgautam@gmail.com>
2020-08-14 12:36:57 +0545

    transfered directory state into redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,20 +1,22 @@
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
 
-import { persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-import userReducer from "./user/user-reducer";
-import cartReducer from "./cart/cart-reducer";
+import userReducer from './user/user-reducer';
+import cartReducer from './cart/cart-reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
-  key: "root",
+  key: 'root',
   storage,
-  whitelist: ["cart"],
+  whitelist: ['cart'],
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
